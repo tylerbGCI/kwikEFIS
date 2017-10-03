08649605ca8382a18e37f0f515447e9f16b8a0a3
ninelima <ninelima@yahoo.com>
2017-10-03 21:27:52 +0800

    Add to VCS

---
+++ ulib/src/main/java/player/efis/common/AirspaceClass.java
@@ -0,0 +1,26 @@
+package player.efis.common;
+
+import java.util.*;
+
+
+enum AirspaceClass
+{
+    CLASSA,
+    CLASSB,
+    CLASSC,
+    CLASSD,
+    CLASSE,
+    CLASSF,
+    CLASSG,
+    RESTRICT,
+    DANGER,
+    PROHIBITED,
+    WAVE,
+    NOGLIDER,
+    CTR,
+    TMZ,
+    RMZ,
+    MATZ,
+    GWAVE,
+    OTHER
+}
