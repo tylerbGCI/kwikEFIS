26974cad8f20ccc9b271f28e1bed3d1e58c10826
ninelima <ninelima@yahoo.com>
2018-09-07 12:15:29 +0800

    Make the build script a bit more generic

--- amake.sh
+++ amake.sh
@@ -29,18 +29,14 @@ if [ ! -z $1 ] && [ $1 == '-w' ]
 then
     echo 'wifi nexus 7'
     adb devices
-    #gsm nexus 7
-    #adb -s 015d3249295c160b install -r ./kwik-efis.apk
-    #adb -s 015d3249295c160b install -r ./kwik-dmap.apk
-    #adb -s 015d3249295c160b install -r ./kwik-efis-datapac-zar.aus.apk
-    #adb -s 015d3249295c160b install -r ./kwik-efis-datapac-usa.can.apk
-    #adb -s 015d3249295c160b install -r ./kwik-efis-datapac-eur.rus.apk
     
     #wifi nexus 7
-    #adb -s 015d2ea4a467ec11 uninstall player.efis.pfd
-    #adb -s 015d2ea4a467ec11 uninstall player.efis.mfd
+    adb -s 015d2ea4a467ec11 uninstall player.efis.pfd
+    adb -s 015d2ea4a467ec11 uninstall player.efis.mfd
+    
     adb -s 015d2ea4a467ec11 install -r ./kwik-efis.apk
     adb -s 015d2ea4a467ec11 install -r ./kwik-dmap.apk
+    
     #adb -s 015d2ea4a467ec11 install -r ./kwik-efis-datapac-zar.aus.apk
 fi
 
@@ -48,19 +44,17 @@ if [ ! -z $1 ] && [ $1 == '-g' ]
 then
     echo 'gsm nexus 7'
     adb devices
+    
     #gsm nexus 7
+    adb -s 015d3249295c160b uninstall player.efis.pfd
+    adb -s 015d3249295c160b uninstall player.efis.mfd
+    
     adb -s 015d3249295c160b install -r ./kwik-efis.apk
     adb -s 015d3249295c160b install -r ./kwik-dmap.apk
+    
     #adb -s 015d3249295c160b install -r ./kwik-efis-datapac-zar.aus.apk
     #adb -s 015d3249295c160b install -r ./kwik-efis-datapac-usa.can.apk
     #adb -s 015d3249295c160b install -r ./kwik-efis-datapac-eur.rus.apk
-    
-    #wifi nexus 7
-    #adb -s 015d2ea4a467ec11 uninstall player.efis.pfd
-    #adb -s 015d2ea4a467ec11 uninstall player.efis.mfd
-    #adb -s 015d2ea4a467ec11 install -r ./kwik-efis.apk
-    #adb -s 015d2ea4a467ec11 install -r ./kwik-dmap.apk
-    #adb -s 015d2ea4a467ec11 install -r ./kwik-efis-datapac-zar.aus.apk
 fi
 
 
