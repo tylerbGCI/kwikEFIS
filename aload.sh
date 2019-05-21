6e3329e1eb91cd9def57915380d8c33d8dceefb9
ninelima <ninelima@yahoo.com>
2019-05-21 15:42:10 +0800

    wip

--- aload.sh
+++ aload.sh
@@ -9,52 +9,32 @@ cd ./apk
 if [ ! -z $1 ] && [ $1 == '-w' ] 
 then
     echo 'wifi nexus 7'
-    adb devices
-    
-    #wifi nexus 7
-    adb -s 015d2ea4a467ec11 uninstall player.efis.pfd
-    adb -s 015d2ea4a467ec11 uninstall player.efis.mfd
-    
-    adb -s 015d2ea4a467ec11 install -r ./kwik-efis.apk
-    adb -s 015d2ea4a467ec11 install -r ./kwik-dmap.apk
-    
-    #adb -s 015d2ea4a467ec11 install -r ./kwik-efis-datapac-zar.aus.apk
+	DROIDDEVICE=015d2ea4a467ec11
 fi
 
 if [ ! -z $1 ] && [ $1 == '-g' ] 
 then
     echo 'gsm nexus 7'
-    adb devices
-    
-    #gsm nexus 7
-    adb -s 015d3249295c160b uninstall player.efis.pfd
-    adb -s 015d3249295c160b uninstall player.efis.mfd
-    
-    adb -s 015d3249295c160b install -r ./kwik-efis.apk
-    adb -s 015d3249295c160b install -r ./kwik-dmap.apk
-    
-    #adb -s 015d3249295c160b install -r ./kwik-efis-datapac-zar.aus.apk
-    #adb -s 015d3249295c160b install -r ./kwik-efis-datapac-usa.can.apk
-    #adb -s 015d3249295c160b install -r ./kwik-efis-datapac-eur.rus.apk
+	DROIDDEVICE=015d3249295c160b
 fi
-
 
 if [ ! -z $1 ] && [ $1 == '-s' ] 
 then
     echo 'samsung g5'
-    adb devices
-    
-    #gsm nexus 7
-    adb -s 758f9cc3 uninstall player.efis.pfd
-    adb -s 758f9cc3 uninstall player.efis.mfd
-    
-    adb -s 758f9cc3 install -r ./kwik-efis.apk
-    adb -s 758f9cc3 install -r ./kwik-dmap.apk
-    
-    #adb -s 758f9cc3 install -r ./kwik-efis-datapac-zar.aus.apk
-    #adb -s 758f9cc3 install -r ./kwik-efis-datapac-usa.can.apk
-    #adb -s 758f9cc3 install -r ./kwik-efis-datapac-eur.rus.apk
+	DROIDDEVICE=758f9cc3
 fi
+
+adb -s $DROIDDEVICE uninstall player.efis.pfd
+adb -s $DROIDDEVICE uninstall player.efis.mfd
+adb -s $DROIDDEVICE uninstall player.efis.cfd
+
+adb -s $DROIDDEVICE install -r ./kwik-efis.apk
+adb -s $DROIDDEVICE install -r ./kwik-dmap.apk
+adb -s $DROIDDEVICE install -r ./kwik-comp.apk
+
+#adb -s DROIDDEVICE install -r ./kwik-efis-datapac-zar.aus.apk
+#adb -s DROIDDEVICE install -r ./kwik-efis-datapac-usa.can.apk
+#adb -s DROIDDEVICE install -r ./kwik-efis-datapac-eur.rus.apk
 
 
 #pskill java
