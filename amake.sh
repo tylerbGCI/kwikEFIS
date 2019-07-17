7e4d678aaeaea241b7190ffcc586bf3eaab8b5ca
TacoTheDank <SkytkRSfan3895@gmail.com>
2019-07-17 15:19:58 -0400

    Clean up and update gradle files
    Signed-off-by: TacoTheDank <SkytkRSfan3895@gmail.com>

--- amake.sh
+++ amake.sh
@@ -13,17 +13,17 @@ rm ./apk/kwik-efis-datapac-eur.rus.apk
 rm ./apk/kwik-efis-datapac-sah.jap.apk
 rm ./apk/kwik-efis-datapac-pan.arg.apk
 
-#./gradlew build
+# ./gradlew build
 ./gradlew assemble
 
 cp ./pfd/build/outputs/apk/debug/pfd-debug.apk ./apk/kwik-efis.apk
 cp ./mfd/build/outputs/apk/debug/mfd-debug.apk ./apk/kwik-dmap.apk
 cp ./cfd/build/outputs/apk/debug/cfd-debug.apk ./apk/kwik-comp.apk
-cp ./data.zar.aus/build/outputs/apk/debug/data.zar.aus-debug.apk ./apk/kwik-efis-datapac-zar.aus.apk
-cp ./data.usa.can/build/outputs/apk/debug/data.usa.can-debug.apk ./apk/kwik-efis-datapac-usa.can.apk
-cp ./data.eur.rus/build/outputs/apk/debug/data.eur.rus-debug.apk ./apk/kwik-efis-datapac-eur.rus.apk
-cp ./data.sah.jap/build/outputs/apk/debug/data.sah.jap-debug.apk ./apk/kwik-efis-datapac-sah.jap.apk
-cp ./data.pan.arg/build/outputs/apk/debug/data.pan.arg-debug.apk ./apk/kwik-efis-datapac-pan.arg.apk
+cp ./datapac/zar.aus/build/outputs/apk/debug/zar.aus-debug.apk ./apk/kwik-efis-datapac-zar.aus.apk
+cp ./datapac/usa.can/build/outputs/apk/debug/usa.can-debug.apk ./apk/kwik-efis-datapac-usa.can.apk
+cp ./datapac/eur.rus/build/outputs/apk/debug/eur.rus-debug.apk ./apk/kwik-efis-datapac-eur.rus.apk
+cp ./datapac/sah.jap/build/outputs/apk/debug/sah.jap-debug.apk ./apk/kwik-efis-datapac-sah.jap.apk
+cp ./datapac/pan.arg/build/outputs/apk/debug/pan.arg-debug.apk ./apk/kwik-efis-datapac-pan.arg.apk
 cp ./CHANGELOG.md ./apk/CHANGELOG.md
 
 exit
