d0541dcdb24e805d03b8f49bbc41cf368e4d1ae6
ninelima <ninelima@yahoo.com>
2019-07-20 23:33:20 +0800

    Update scripts

--- amake.sh
+++ amake.sh
@@ -19,12 +19,14 @@ rm ./apk/kwik-efis-datapac-pan.arg.apk
 cp ./pfd/build/outputs/apk/debug/pfd-debug.apk ./apk/kwik-efis.apk
 cp ./mfd/build/outputs/apk/debug/mfd-debug.apk ./apk/kwik-dmap.apk
 cp ./cfd/build/outputs/apk/debug/cfd-debug.apk ./apk/kwik-comp.apk
-cp ./datapac/zar.aus/build/outputs/apk/debug/zar.aus-debug.apk ./apk/kwik-efis-datapac-zar.aus.apk
-cp ./datapac/usa.can/build/outputs/apk/debug/usa.can-debug.apk ./apk/kwik-efis-datapac-usa.can.apk
-cp ./datapac/eur.rus/build/outputs/apk/debug/eur.rus-debug.apk ./apk/kwik-efis-datapac-eur.rus.apk
-cp ./datapac/sah.jap/build/outputs/apk/debug/sah.jap-debug.apk ./apk/kwik-efis-datapac-sah.jap.apk
-cp ./datapac/pan.arg/build/outputs/apk/debug/pan.arg-debug.apk ./apk/kwik-efis-datapac-pan.arg.apk
+cp ./datapac/build/outputs/apk/eur_rus/debug/datapac-eur_rus-debug.apk ./apk/kwik-efis-datapac-eur.rus.apk
+cp ./datapac/build/outputs/apk/pan_arg/debug/datapac-pan_arg-debug.apk ./apk/kwik-efis-datapac-pan.arg.apk
+cp ./datapac/build/outputs/apk/sah_jap/debug/datapac-sah_jap-debug.apk ./apk/kwik-efis-datapac-sah.jap.apk
+cp ./datapac/build/outputs/apk/usa_can/debug/datapac-usa_can-debug.apk ./apk/kwik-efis-datapac-usa.can.apk
+cp ./datapac/build/outputs/apk/zar_aus/debug/datapac-zar_aus-debug.apk ./apk/kwik-efis-datapac-zar.aus.apk
 cp ./CHANGELOG.md ./apk/CHANGELOG.md
 
 exit
 
+
+h:/src/kwikEFIS/datapac/build/outputs/apk/eur_rus/debug/datapac-eur_rus-debug.apk 
